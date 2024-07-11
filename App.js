ac23be59c6420eda5d0de8fbbfa3aa676ecb6825
Jayesh Purswani <133760851+jerry-xon@users.noreply.github.com>
2024-07-11 19:20:34 +0530

    app.js recleaned

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>vnadlfkbaef;aef v'aodf adkjh</Text>
+      <Text>jayesh</Text>
       <StatusBar style="auto" />
     </View>
   );
